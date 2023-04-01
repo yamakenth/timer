c70d55d91918c08915e72b0269fcac9077627d22
Ken Yamada <yamakenth@gmail.com>
2023-04-01 18:48:54 -0400

    Finish implementing the functionality of InputPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,54 @@
-import React from "react";
+import { CssBaseline, Theme, ThemeProvider, createTheme } from "@mui/material";
+import React, { useState } from "react";
+import InputPage from "./components/InputPage";
+
+const darkTheme: Theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const initialTime: Number[] = new Array(6).fill(0);
 
 function App() {
+  const [time, setTime] = useState(initialTime);
+  const [countdown, setCountdown] = useState(false);
+
+  function handleNumberInput(
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void {
+    const val = (event.target as Element).innerHTML;
+
+    let newTime;
+    if (val !== "00") {
+      newTime = [...time, Number(val)].slice(1);
+    } else {
+      newTime = [...time, 0, 0].slice(2);
+    }
+    setTime(newTime);
+  }
+
+  function handleBackspace(): void {
+    const newTime = [...time];
+    newTime.unshift(0);
+    newTime.pop();
+    setTime(newTime);
+  }
+
   return (
     <div className="App">
-      <div>Hello</div>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        {countdown ? (
+          <div>countdown</div>
+        ) : (
+          <InputPage
+            time={time}
+            handleNumberInput={handleNumberInput}
+            handleBackspace={handleBackspace}
+          />
+        )}
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/InputPage.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const inputValues: string[] = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "00",
+  "0",
+];
+
+interface InputPageProps {
+  time: Number[];
+  handleNumberInput: React.MouseEventHandler<HTMLButtonElement>;
+  handleBackspace: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function inputPage({
+  time,
+  handleNumberInput,
+  handleBackspace,
+}: InputPageProps) {
+  return (
+    <>
+      <div>icon: Timer</div>
+      <div>{`
+      ${time.slice(0, 2).join("")}h
+      ${time.slice(2, 4).join("")}m
+      ${time.slice(4, 6).join("")}s`}</div>
+      <div>
+        {inputValues.map((val, idx) => (
+          <button key={idx} onClick={handleNumberInput}>
+            {val}
+          </button>
+        ))}
+        <button onClick={handleBackspace}>&lt;</button>
+      </div>
+    </>
+  );
+}
